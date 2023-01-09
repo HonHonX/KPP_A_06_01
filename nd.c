a82ada16502cb4c2f7a6d9cf45612800e9ba1bd9
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-09 11:42:59 +0100

    Create nd.c

---
+++ nd.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+// Funktions-Prototypen
+
+
+//Hauptprogramm
+int main () {
+
+	/*
+	* Berechnung des Notendurchschnitts über ein Array
+	*/
+
+	printf("\n *********************************** ");
+	printf("\n *   ^,,,,^   *                    * ");
+	printf("\n *  ( o x o)  *  Notendurchschnitt * ");
+	printf("\n *  ( 7   7)  *                    * ");
+	printf("\n *   ´´´´´´   *            ©S.Buch * ");
+	printf("\n *********************************** \n\n");
+	
+	//Variablen
+  int i, noten[10];
+  float notenberechnung=0;
+  
+  //Werte einlesen
+  for (i=1; i<=10; i++) {
+    printf("Bitte geben Sie die &d.Zahl ein: \n",i;);
+    scanf("%d", &noten[i]);
+    notenberechnung += noten[i];
+  }
+  
+  //Wert ausgeben
+  printf("\nDer Notendurchschnitt lautet %f.\n", notenberechnung/10);    
+  
+	//Ende
+	printf("\n\n");
+	return 0;
+}
+
+//Funktionen
