db88778ed5d9e3f2f24a5406bfa124cd6a07383e
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-09 18:42:02 +0100

    Update nd.c

--- nd.c
+++ nd.c
@@ -15,20 +15,21 @@ int main () {
 	printf("\n *********************************** \n\n");
 	
 	//Variablen
-  int i, noten[10];
-  float notenberechnung=0;
+ 	int i, noten[10];
+  	float notenberechnung=0;
+	printf("Dieses Programm liest zehn Zahlen(Noten) ein, aus denen der Durchschnitt berechnet werden soll\n");
   
-  //Werte einlesen
-  for (i=0; i<10; i++) {
-    printf("Bitte geben Sie die %d.Zahl ein: \n",i+1);
-    scanf("%d", &noten[i]);
-    notenberechnung += noten[i];
-  }
+  	//Werte einlesen
+ 	for (i=0; i<10; i++) {
+    		printf("Bitte geben Sie die %d.Zahl ein: \n",i+1);
+    		scanf("%d", &noten[i]);
+   		notenberechnung += noten[i];
+ 	}
   
-  //Wert ausgeben
-  printf("\nDer Notendurchschnitt lautet %.2f.\n", notenberechnung/10);    
+  	//Wert ausgeben
+  	printf("\nDer Notendurchschnitt lautet %.2f.\n", notenberechnung/10);    
   
-  //Ende
-  printf("\n\n");
-  return 0;
+  	//Ende
+  	printf("\n\n");
+  	return 0;
 }
